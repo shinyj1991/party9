82063af488812d207e39c2dce66b0416d7be4693
simplizm-company <40747170+simplizm-company@users.noreply.github.com>
2020-08-24 11:21:55 +0900

    index

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <article id="index">
+    hello
+  </article>
+</template>
+
+<script>
+  import { mapState, mapMutations, mapActions } from 'vuex';
+
+  export default {
+    data: () => ({
+      title: ''
+    }),
+    methods: {}
+    
+  }
+</script>
+
+<style lang="scss">
+#index {}
+</style>
